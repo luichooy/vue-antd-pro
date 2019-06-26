de9af9f973a2aeedcc30c43801024f7e821a0b27
luichooy <luichooy@163.com>
2019-06-26 19:53:03 +0800

    Add Trend component

--- src/components/Trend/index.vue
+++ src/components/Trend/index.vue
@@ -1,13 +1,63 @@
-<template>
-  $END$
-</template>
-
 <script>
 export default {
-  name: 'index'
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    value: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      default: 'up'
+    }
+  },
+  render () {
+    const { label, value, type } = this
+    const iconClass = ['trend-icon', `trend-icon-${type}`]
+    const iconType = `caret-${type}`
+    return (
+      <div class="trend">
+        <span class="trend-label">{ label }</span>
+        <span class="trend-value">{ value }%</span>
+        <span class={ iconClass }>
+          <a-icon type={ iconType } />
+        </span>
+      </div>
+    )
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+  .trend {
+    display: inline-block;
+    line-height: 22px;
+    .trend-label {
+      margin-right: 8px;
+    }
+    
+    .trend-value {
+      color: rgba(0, 0, 0, 0.85);
+    }
+    
+    .trend-icon {
+      margin-left: 4px;
+      
+      &.trend-icon-up {
+        color: rgb(245, 34, 45);
+      }
+      
+      &.trend-icon-down {
+        color: rgb(82, 196, 26);
+      }
+      
+      i {
+        font-size: 12px;
+        transform: scale(0.83);
+      }
+    }
+  }
 </style>
