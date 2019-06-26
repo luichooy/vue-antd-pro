456aed5102dadd0290868a513f9652b5242254da
luichooy <luichooy@163.com>
2019-06-26 14:57:33 +0800

    Add color.less & format Setting code

--- src/components/Setting/index.vue
+++ src/components/Setting/index.vue
@@ -11,22 +11,22 @@
       <div class="setting-drawer-content">
         <div class="setting-block">
           <h3 class="block-title">整体风格设置</h3>
-
+          
           <div class="block-content">
             <a-tooltip v-for="theme in menuThemeList" :key="theme.value" :title="theme.name">
               <div class="img-block" @click="changeMenuTheme(theme.value)">
                 <img :src="theme.imgSrc" :alt="theme.value">
                 <div class="img-block-checked" v-if="menuTheme === theme.value">
-                  <a-icon type="check"/>
+                  <a-icon type="check" />
                 </div>
               </div>
             </a-tooltip>
           </div>
         </div>
-
+        
         <div class="setting-block">
           <h3 class="block-title">主题色</h3>
-
+          
           <div class="block-content color-theme">
             <a-tooltip
               v-for="color in colorList"
@@ -40,22 +40,22 @@
             </a-tooltip>
           </div>
         </div>
-        <a-divider/>
-
+        <a-divider />
+        
         <div class="setting-block">
           <h3 class="block-title">导航模式</h3>
-
+          
           <div class="block-content">
             <a-tooltip v-for="layout in layoutModeList" :key="layout.value" :title="layout.name">
               <div class="img-block" @click="changeLayout(layout.value)">
                 <img :src="layout.imgSrc" :alt="layout.name">
                 <div class="img-block-checked" v-if="layoutMode === layout.value">
-                  <a-icon type="check"/>
+                  <a-icon type="check" />
                 </div>
               </div>
             </a-tooltip>
           </div>
-
+          
           <div style="margin-top: 24px;">
             <a-list :split="false">
               <a-list-item>
@@ -70,9 +70,9 @@
                     <a-select-option value="fluid" v-if="layoutMode !== 'side'">流式</a-select-option>
                   </a-select>
                 </a-tooltip>
-                <a-list-item-meta title="内容区域宽度"/>
+                <a-list-item-meta title="内容区域宽度" />
               </a-list-item>
-
+              
               <a-list-item>
                 <a-switch
                   slot="actions"
@@ -80,9 +80,9 @@
                   :defaultChecked="fixedHeader"
                   @change="handleFixedHeader"
                 />
-                <a-list-item-meta title="固定 Header"/>
+                <a-list-item-meta title="固定 Header" />
               </a-list-item>
-
+              
               <a-list-item>
                 <a-switch
                   slot="actions"
@@ -95,10 +95,11 @@
                   <div
                     slot="title"
                     :style="{ textDecoration: !fixedHeader ? 'line-through' : 'unset' }"
-                  >下滑时隐藏 Header</div>
+                  >下滑时隐藏 Header
+                  </div>
                 </a-list-item-meta>
               </a-list-item>
-
+              
               <a-list-item>
                 <a-switch
                   slot="actions"
@@ -111,14 +112,15 @@
                   <div
                     slot="title"
                     :style="{ textDecoration: layoutMode === 'top' ? 'line-through' : 'unset' }"
-                  >固定侧边菜单</div>
+                  >固定侧边菜单
+                  </div>
                 </a-list-item-meta>
               </a-list-item>
             </a-list>
           </div>
         </div>
-        <a-divider/>
-
+        <a-divider />
+        
         <div class="setting-block">
           <h3 class="block-title">其他设置</h3>
           <div>
@@ -135,192 +137,190 @@
             </a-list>
           </div>
         </div>
-        <a-divider/>
+        <a-divider />
       </div>
       <div v-if="visible || triggerVisible" class="setting-drawer-trigger" @click="toggle">
-        <a-icon type="setting" v-if="!visible"/>
-        <a-icon type="close" v-else/>
+        <a-icon type="setting" v-if="!visible" />
+        <a-icon type="close" v-else />
       </div>
     </a-drawer>
   </div>
 </template>
 
 <script>
-  import { appStoreMixin } from '@/mixins'
-  import {
-    menuThemeList,
-    colorList,
-    layoutModeList,
-    updateTheme,
-    updateColorWeak
-  } from './tools'
-  import config from '@/store/config'
-  import { throttle } from '@/utils/optimize'
+import { appStoreMixin } from '@/mixins'
+import {
+  menuThemeList,
+  colorList,
+  layoutModeList,
+  updateTheme,
+  updateColorWeak
+} from './tools'
+import config from '@/store/config'
+import { throttle } from '@/utils/optimize'
 
-  const {
-    app: { color, colorWeak }
-  } = config
+const {
+  app: { color, colorWeak }
+} = config
 
-  export default {
-    mixins: [appStoreMixin],
-    data() {
-      return {
-        menuThemeList,
-        colorList,
-        layoutModeList,
-        visible: true,
-        triggerVisible: false
-      }
-    },
-    methods: {
-      changeMenuTheme(theme) {
-        this.set_menuTheme(theme)
-        // this.baseConfig.navTheme = theme;
-        // this.$store.dispatch('ToggleTheme', theme);
-      },
-      changeColorTheme(color) {
-        if (this.color !== color) {
-          this.set_color(color)
-          updateTheme(color)
-        }
-      },
-      changeLayout(layout) {
-        this.set_layout(layout)
-      },
-      changeContentWidth(type) {
-        this.set_contentWidth(type)
-      },
-      handleFixedHeader(fixed) {
-        this.set_fixedHeader(fixed)
-      },
-      handleFixedHeaderHidden(isHidden) {
-        this.set_autoHideHeader(isHidden)
-      },
-      handleFixSiderbar(fixed) {
-        this.set_fixedSiderbar(fixed)
-      },
-      changeColorWeak(colorWeak) {
-        this.set_colorWeak(colorWeak)
-        updateColorWeak(colorWeak)
-      },
-      toggle() {
-        this.visible = !this.visible
-      },
-      onClose() {
-        this.visible = false
-      },
-      handleMousemove(e) {
-        const fn = () => {
-          const pageX = document.documentElement.offsetWidth
-          const currentX = e.x
-
-          if (pageX - currentX < 100) {
-            this.triggerVisible = true
-          } else {
-            this.triggerVisible = false
-          }
-        }
-
-        return throttle(fn, 500).call(this)
-      }
-    },
-    mounted() {
-      const vm = this
-      setTimeout(() => {
-        vm.visible = false
-      }, 16)
-
-      if (this.defaultColor !== color) {
-        updateTheme(this.defaultColor)
-      }
-      if (this.colorWeak !== colorWeak) {
-        updateColorWeak(this.colorWeak)
-      }
-
-      window.addEventListener('mousemove', this.handleMousemove, false)
-    },
-    beforeDestroy() {
-      window.removeEventListener('mousemove', this.handleMousemove, false)
+export default {
+  mixins: [appStoreMixin],
+  data () {
+    return {
+      menuThemeList,
+      colorList,
+      layoutModeList,
+      visible: true,
+      triggerVisible: false
     }
+  },
+  methods: {
+    changeMenuTheme (theme) {
+      this.set_menuTheme(theme)
+    },
+    changeColorTheme (color) {
+      if (this.color !== color) {
+        this.set_color(color)
+        updateTheme(color)
+      }
+    },
+    changeLayout (layout) {
+      this.set_layout(layout)
+    },
+    changeContentWidth (type) {
+      this.set_contentWidth(type)
+    },
+    handleFixedHeader (fixed) {
+      this.set_fixedHeader(fixed)
+    },
+    handleFixedHeaderHidden (isHidden) {
+      this.set_autoHideHeader(isHidden)
+    },
+    handleFixSiderbar (fixed) {
+      this.set_fixedSiderbar(fixed)
+    },
+    changeColorWeak (colorWeak) {
+      this.set_colorWeak(colorWeak)
+      updateColorWeak(colorWeak)
+    },
+    toggle () {
+      this.visible = !this.visible
+    },
+    onClose () {
+      this.visible = false
+    },
+    handleMousemove (e) {
+      const fn = () => {
+        const pageX = document.documentElement.offsetWidth
+        const currentX = e.x
+        
+        if (pageX - currentX < 100) {
+          this.triggerVisible = true
+        } else {
+          this.triggerVisible = false
+        }
+      }
+      
+      return throttle(fn, 500).call(this)
+    }
+  },
+  mounted () {
+    const vm = this
+    setTimeout(() => {
+      vm.visible = false
+    }, 16)
+    
+    if (this.defaultColor !== color) {
+      updateTheme(this.defaultColor)
+    }
+    if (this.colorWeak !== colorWeak) {
+      updateColorWeak(this.colorWeak)
+    }
+    
+    window.addEventListener('mousemove', this.handleMousemove, false)
+  },
+  beforeDestroy () {
+    window.removeEventListener('mousemove', this.handleMousemove, false)
   }
+}
 </script>
 
 <style lang="scss" scoped>
-.setting-drawer-content {
-  .setting-block {
-    margin-bottom: 24px;
-
-    .block-content {
-      display: flex;
-
-      .img-block {
-        position: relative;
-        margin-right: 16px;
-        border-radius: 4px;
-        cursor: pointer;
-
-        img {
-          width: 48px;
-        }
-
-        .img-block-checked {
-          position: absolute;
-          top: 0;
-          right: 0;
-          width: 100%;
-          padding-top: 15px;
-          padding-left: 24px;
-          height: 100%;
-          color: #1890ff;
-          font-size: 14px;
-          font-weight: 700;
-        }
-      }
-
-      &.color-theme {
-        height: 20px;
-        .color-theme-item {
-          width: 20px;
-          height: 20px;
-          float: left;
-          margin-right: 8px;
-          padding-left: 0;
-          padding-right: 0;
-          border-radius: 2px;
+  .setting-drawer-content {
+    .setting-block {
+      margin-bottom: 24px;
+      
+      .block-content {
+        display: flex;
+        
+        .img-block {
+          position: relative;
+          margin-right: 16px;
+          border-radius: 4px;
           cursor: pointer;
-          text-align: center;
-          color: #fff;
-          font-weight: 700;
-
-          i {
+          
+          img {
+            width: 48px;
+          }
+          
+          .img-block-checked {
+            position: absolute;
+            top: 0;
+            right: 0;
+            width: 100%;
+            padding-top: 15px;
+            padding-left: 24px;
+            height: 100%;
+            color: #1890ff;
             font-size: 14px;
+            font-weight: 700;
+          }
+        }
+        
+        &.color-theme {
+          height: 20px;
+          .color-theme-item {
+            width: 20px;
+            height: 20px;
+            float: left;
+            margin-right: 8px;
+            padding-left: 0;
+            padding-right: 0;
+            border-radius: 2px;
+            cursor: pointer;
+            text-align: center;
+            color: #fff;
+            font-weight: 700;
+            
+            i {
+              font-size: 14px;
+            }
           }
         }
       }
     }
   }
-}
-
-.setting-drawer-trigger {
-  position: absolute;
-  top: 240px;
-  right: 300px;
-  width: 48px;
-  height: 48px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 1001;
-  text-align: center;
-  font-size: 16px;
-  border-radius: 4px 0 0 4px;
-  background: #1890ff;
-  cursor: pointer;
-  pointer-events: auto;
-
-  i {
-    color: rgb(255, 255, 255);
-    font-size: 20px;
+  
+  .setting-drawer-trigger {
+    position: absolute;
+    top: 240px;
+    right: 300px;
+    width: 48px;
+    height: 48px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1001;
+    text-align: center;
+    font-size: 16px;
+    border-radius: 4px 0 0 4px;
+    background: #1890ff;
+    cursor: pointer;
+    pointer-events: auto;
+    
+    i {
+      color: rgb(255, 255, 255);
+      font-size: 20px;
+    }
   }
-}
 </style>
