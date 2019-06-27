fd5dad4dfec75ae7280e8c00c79a01cb3671f245
luichooy <luichooy@163.com>
2019-06-27 16:36:16 +0800

    Create MiniBar component

--- src/components/Chart/MiniBar.vue
+++ src/components/Chart/MiniBar.vue
@@ -1,13 +1,29 @@
-<template>
-  $END$
-</template>
-
 <script>
+import moment from 'moment'
+
+const data = []
+for (let i = 9; i >= 0; i--) {
+  data.push({
+    day: moment().subtract(i, 'days').format('YYYY/MM/DD'),
+    count: Math.round(Math.random() * 100)
+  })
+}
 export default {
-  name: 'MiniBar'
+  name: 'MiniBar',
+  data () {
+    return {
+      data
+    }
+  },
+  render () {
+    const { data } = this
+    
+    return (
+      <v-chart data={ data } forceFit={ true } height={ 46 } padding={ [10, 5, 0, 5] }>
+        <v-tooltip />
+        <v-bar position="day*count" />
+      </v-chart>
+    )
+  }
 }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
