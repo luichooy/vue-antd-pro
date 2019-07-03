e0a1f1c3776dc824b2cb939a04b3f7fe4a7e6ee6
luichooy <luichooy@163.com>
2019-07-03 16:08:26 +0800

    Create OfflineData component

---
+++ src/views/dashboard/components/OfflineData.vue
@@ -0,0 +1,38 @@
+<script>
+import CustomTab from './CustomTab'
+
+export default {
+  name: 'OfflineData',
+  components: { CustomTab },
+  props: {
+    loading: Boolean,
+    offlineData: {
+      type: Array,
+      required: true
+    }
+  },
+  render () {
+    const {
+      loading,
+      offlineData
+    } = this
+    
+    return (
+      <a-card loading={ loading } bordered={ false } style="margin-top: 16px;">
+        <a-tabs>
+          { offlineData.map(shop => (
+            <a-tab-pane key={ shop.name }>
+              <template slot="tab">
+                <custom-tab data={ shop } />
+              </template>
+              <div style={ { padding: '0 24px' } }>
+                123
+              </div>
+            </a-tab-pane>
+          )) }
+        </a-tabs>
+      </a-card>
+    )
+  }
+}
+</script>
