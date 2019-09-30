0d0d0f890cbf22c0c6d448d029a8913fd61656b2
luichooy <luichooy@163.com>
2019-09-30 15:21:02 +0800

    Add: stylelint config

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: [],
+  extends: 'stylelint-config-standard',
   plugins: [],
   processors: [],
   //   defaultSeverity: 'error', //指定应用到所有rules上的严格程度，可选值有‘warning','error'
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  '*.{js,jsx,vue}': ['yarn lint', 'git add']
+  '*.{js,jsx,vue}': ['yarn lint', 'git add'],
+  '*.{css}': ['stylelint --fix', 'git add'],
+  '*.{scss}': ['stylelint --syntax=scss --fix', 'git add'],
+  '*.{vue}': ['stylelint --fix', 'git add'],
+  '*.{json}': ['prettier --write', 'git add']
 }
