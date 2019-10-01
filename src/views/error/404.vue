cb022503bd26155b0e24583fa9de2bd234108d8d
luichooy <luichooy@163.com>
2019-10-01 12:19:33 +0800

    Fix: stylelint

--- src/views/error/404.vue
+++ src/views/error/404.vue
@@ -9,15 +9,14 @@
             @click="$router.replace('/')"
             type="button"
             class="btn btn-outline-white"
-          >←&nbsp; 返回首页
-          </button>
+          >←&nbsp; 返回首页</button>
         </div>
         <div class="col-md-6 order-1 order-md-2 text-center mb-5 mb-md-0">
-          <div class="ui-device macbook w-100 rounded" style="max-width: 500px">
-            <img class="device-img" src="../../assets/images/macbook-gold.png" alt>
+          <div class="ui-device macbook w-100 rounded" style="max-width: 500px;">
+            <img class="device-img" src="../../assets/images/macbook-gold.png" alt />
             <div class="device-content">
               <div class="error-device-content bg-primary">
-                <img class="w-75 position-relative" src="../../assets/images/404.png" alt>
+                <img class="w-75 position-relative" src="../../assets/images/404.png" alt />
                 <!--<div class="error-device-code text-white font-weight-bolder">404</div>-->
               </div>
             </div>
@@ -33,16 +32,16 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
-  .error-device-content {
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-    line-height: 1;
-  }
+.error-device-content {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  line-height: 1;
+}
 </style>
