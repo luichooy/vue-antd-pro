cef849b8a7d91a5cd0204f937e470cbd20baaa71
luichooy <luichooy@163.com>
2019-11-09 14:49:33 +0800

    stylelint: no-empty-source

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -4,6 +4,7 @@ module.exports = {
   processors: [],
   //   defaultSeverity: 'error', //指定应用到所有rules上的严格程度，可选值有‘warning','error'
   rules: {
-    'font-family-no-missing-generic-family-keyword': null
+    'font-family-no-missing-generic-family-keyword': null,
+    'no-empty-source': 'off'
   }
 }
