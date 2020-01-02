f3f618c74416d62d5d4ab6c75872aa0a625b9368
luichooy <luichooy@gmail.com>
2020-01-02 20:02:04 +0800

    feat: g2-bar

--- src/views/g2/bar.vue
+++ src/views/g2/bar.vue
@@ -1,11 +1,33 @@
 <template>
   <div class="bar">
-    g2-bar
+    <a-card :bordered="false" title="基本示例">
+      <div id="basic-bar"></div>
+    </a-card>
   </div>
 </template>
 
 <script>
+import G2 from '@antv/g2'
+
+const data = [
+  { genre: 'Sports', sold: 275 },
+  { genre: 'Strategy', sold: 115 },
+  { genre: 'Action', sold: 120 },
+  { genre: 'Shooter', sold: 350 },
+  { genre: 'Other', sold: 150 }
+]
+
 export default {
-  components: {}
+  mounted () {
+    const chart = new G2.Chart({
+      container: 'basic-bar',
+      forceFit: true,
+      height: 500
+    })
+
+    chart.source(data)
+    chart.interval().position('genre*sold').color('genre')
+    chart.render()
+  }
 }
 </script>
